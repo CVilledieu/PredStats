a0b9ae8d362aca09fbeedf723cf68b02d02ec3a0
Christopher V <ChrisRVill@gmail.com>
2024-03-17 17:27:13 -0700

    Created get fuc to fetch the data and README file

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+func main() {
+	//Time since Epoch
+	now := time.Now()
+	Epoch := strconv.FormatInt(now.Unix(), 10)
+
+	//End point url should end with time since Epoch
+	MatchesUrl := "https://backend.production.omeda-aws.com/api/public/get-matches-since/" + Epoch
+
+	resp, err := http.Get(MatchesUrl)
+	if err != nil {
+		err.Error()
+	}
+
+	resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		err.Error()
+	}
+	fmt.Println(body)
+
+}
