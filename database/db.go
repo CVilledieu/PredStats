b4dd149699cdb8a566b54ac5d66231b4e0dae898
Christopher V <ChrisRVill@gmail.com>
2024-03-24 23:23:39 -0700

    Started database work. Created files and layed out functions

---
+++ database/db.go
@@ -0,0 +1,17 @@
+package database
+
+func postData() {
+	// This function is used to import data from the API into the database
+}
+
+func getData() {
+	// This function is used to get data from the database
+}
+
+func updateData() {
+	// This function is used to update data in the database
+}
+
+func deleteData() {
+	// This function is used to delete data from the database
+}
